18ac3fa6ad867f8f826fc14224d389f70c0b44b1
GEE2424 <gkm18686@gmail.com>
2023-08-21 21:30:39 +0300

    simple code that passes betty checks

---
+++ owl_logo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    printf("        , , ,\n");
+    printf("     ,     \" ,\n");
+    printf("   ,_          _,\n");
+    printf("     \"    ,     \",\n");
+    printf("        , , ,\n");
+    printf("\n");
+    printf("   W H O !\n");
+    printf("\n");
+    
+    return 0;
+}
