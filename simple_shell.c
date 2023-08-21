b386f3089a36c85be8572204fa7e3a93a4e3cadc
GEE2424 <gkm18686@gmail.com>
2023-08-21 23:59:43 +0300

    updated simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -4,8 +4,11 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 #define MAX_INPUT_LENGTH 1024
+
+extern char **environ; /* External environment variables */
 
 int main(void)
 {
@@ -13,6 +16,8 @@ int main(void)
     size_t len = 0;
     ssize_t read;
     pid_t pid;
+    char *path = "/bin"; /* PATH directory */
+    char command_path[MAX_INPUT_LENGTH];
 
     while (1)
     {
@@ -33,18 +38,42 @@ int main(void)
             free(line);
             exit(0);
         }
+        else if (strcmp(line, "env") == 0) /* Print environment variables */
+        {
+            char **env = environ;
+            while (*env != NULL)
+            {
+                printf("%s\n", *env);
+                env++;
+            }
+            continue;
+        }
 
         pid = fork(); /* Create a child process */
 
         if (pid == 0) /* Child process */
         {
-            execlp(line, line, NULL); /* Execute the command */
-            perror("Execution error"); /* Exec only returns if there's an error */
-            exit(1);
+            char *args[MAX_INPUT_LENGTH];
+            int i = 0;
+            char *token = strtok(line, " "); /* Tokenize input */
+
+            while (token != NULL)
+            {
+                args[i++] = token;
+                token = strtok(NULL, " ");
+            }
+            args[i] = NULL; /* Null-terminate the argument array */
+
+            snprintf(command_path, sizeof(command_path), "%s/%s", path, args[0]);
+
+            execve(command_path, args, NULL); /* Execute the command */
+            fprintf(stderr, "Error: %s\n", strerror(errno)); /* Print error if execve fails */
+            exit(errno); /* Exit with the error code */
         }
         else if (pid > 0) /* Parent process */
         {
-            wait(NULL); /* Wait for the child process to finish */
+            int status;
+            wait(&status); /* Wait for the child process to finish */
         }
         else
         {
